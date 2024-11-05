ed6ef9c4b18d3cafece24a26e931fd4bdd75e34a
Naresh Madunala <nareshkumar.madunala@gmail.com>
2024-11-05 14:23:10 -0700

    more on DSA

---
+++ loops/src/main/java/org/madunala/DSA/ReversingLinkedList.java
@@ -0,0 +1,22 @@
+package org.madunala.DSA;
+
+import java.util.LinkedList;
+
+/**
+ * @author nareshmadunala
+ */
+public class ReversingLinkedList {
+
+
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        System.out.println(ll);
+
+        LinkedList<Integer> ll1 = new LinkedList<>();
+        ll.descendingIterator().forEachRemaining(ll1::add);
+        System.out.println(ll1);
+    }
+}
---
+++ loops/src/main/java/org/madunala/genRecursion/FindFactorial.java
@@ -0,0 +1,20 @@
+package org.madunala.genRecursion;
+
+/**
+ * @author nareshmadunala
+ */
+public class FindFactorial {
+
+    public static void main(String[] args) {
+
+        System.out.println(factorial(5));
+    }
+
+
+    public static long factorial(long n) {
+        if (n == 1)
+            return 1;
+        else
+            return (n * factorial(n - 1));
+    }
+}
